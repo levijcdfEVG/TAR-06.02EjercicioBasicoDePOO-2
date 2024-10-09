32af28aa465809ae5d79b0394950ac40e67ffd29
levijcdfEVG <levijosuecandeiasdefigueiredo.guadalupe@alumnado.fundacionloyola.net>
2024-10-09 13:39:36 +0200

    Se han añadido los ficheros base

---
+++ fechas.php
@@ -0,0 +1,11 @@
+<?php 
+
+    class Fechas{
+        protected string $fecha;
+        protected ;
+
+        public function __construct(string $fecha){
+            $this->fecha = $fecha;
+        }
+
+    }
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Análisis de fecha</title>
+    <meta charset="utf-8" />
+    <link href="css/index.css" rel="stylesheet" />
+</head>
+<body>
+    <div>
+        <form action="#">
+            <label for="fecha">Introduce una fecha en formato dd/mm/aa</label><br><br>
+            <input type="text" id="fecha" name="fecha" placeholder="Introduce la fecha aquí" />
+            <input type="submit" id="boton" value="Subir" />
+        </form>
+    </div>
+</body>
+</html>
