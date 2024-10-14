fd1c5720a30de68af4fe686c31ebe8ab3ebfbbd8
levijcdfEVG <levijosuecandeiasdefigueiredo.guadalupe@alumnado.fundacionloyola.net>
2024-10-14 12:21:38 +0200

    Se ha corregido para que devuelva una sola string

--- analizarFecha.php
+++ analizarFecha.php
@@ -7,16 +7,21 @@ $analisisFecha = new Fechas(); // Crea una instancia de la clase Fechas
 // Analiza la fecha y verifica si es un año bisiesto
 $fechaCompuesta = $analisisFecha->esBisiesto($cadenaFecha);
 
+/*
 // Divide el array en día, mes y año
 $dia = $fechaCompuesta['dia'];
 $mes = $fechaCompuesta['mes'];
 $anio = $fechaCompuesta['anio'];
+*/
 
+/*
 // Obtiene el nombre del mes y la cantidad de días
 $infoMes = $analisisFecha->getNombreYDiasMes($mes);
 $nombreMes = $infoMes['nombre'];
 $diasMes = $infoMes['dias'];
+*/
+
 
 // Redirige a una página y pasa los valores en la URL
-header("Location: mostrar.php?dia=$dia&nombreMes=$nombreMes&anio=$anio&diasMes=$diasMes");
+header("Location: mostrar.php?fecha=$fechaCompuesta");
 
--- fechas.php
+++ fechas.php
@@ -41,8 +41,9 @@ class Fechas {
         if ($composicionFecha['mes'] == 2 && $resultado) {
             $this->mes[2]['febrero'] = 29; // Cambiar febrero a 29 días
         }
-
-        return $composicionFecha; // Devolver la composición de la fecha
+        $cadenaFechaFinal = $composicionFecha['dia']." de ".$composicionFecha['dia']." de ".$composicionFecha['anio'];
+        
+        return $cadenaFechaFinal; // Devolver la composición de la fecha
     }
 
     private function analisisFecha($fecha) {
@@ -60,7 +61,7 @@ class Fechas {
         return $fechaAnalizada; // Retornar el array analizado
     }
     
-
+    /*
     public function getNombreYDiasMes($mes) {
         // Obtener el nombre del mes
         $nombreMes = array_keys($this->mes[$mes])[0]; // array_keys() devuelve las claves del array del mes específico; como solo hay una clave (el nombre del mes), usamos [0] para acceder a ella
@@ -77,4 +78,5 @@ class Fechas {
         // Devolver el array con el nombre y los días
         return $infoMes; // Retornar el array separado
     }
+    */    
 }
